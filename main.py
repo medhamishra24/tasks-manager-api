e6b1f2887824dec669105621c204296316b227ec
medhamishra24 <mishramedha976@gmail.com>
2026-07-04 03:42:27 +0530

    feat: connect FastAPI to PostgreSQL with SQLAlchemy, add Task model

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import engine, SessionLocal, Base
+import models
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def home():
     return {"message": "hello, FastAPI!"}
+
+@app.get("/tasks")
+def get_tasks(db: Session = Depends(get_db)):
+    return db.query(models.Task).all()
+
+@app.post("/tasks")
+def add_task(title: str, db: Session = Depends(get_db)):
+    new_task = models.Task(title=title, done=False)
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return new_task
